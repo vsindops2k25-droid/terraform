2f704260a4449c4c465b36f0c6c56084b1586e86
Vijaya Kumar Sinthu - (Internet) <vijayakumar.sinthu@mobileheartbeat.com>
2025-10-24 09:51:22 +0530

    Variables & Data Types

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -6,21 +6,21 @@ resource "aws_instance" "terraform" {
 }
 
 resource "aws_security_group" "allow_all1" {
-  name   = "allow_all1"
+  name   = var.sg_name
  
   
  egress {
-    from_port       = 0  
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port       = var.egress_from_port
+    to_port         = var.egress_to_port
+    protocol        = var.protocol
+    cidr_blocks = var.cidr
   }
 
  ingress {
-    from_port       = 0  
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port       = var.ingress_from_port  
+    to_port         = var.ingress_to_port
+    protocol        = var.protocol
+    cidr_blocks = var.cidr
   }
 
 tags= {
--- variables/variables.tf
+++ variables/variables.tf
@@ -8,14 +8,46 @@ variable "instnace_type" {
     default = "t3.micro"
 }
 
-variable "ec2_tags"{
+variable "ec2_tags" {
     type = map
     default = {
         Name = "Terraform-demo"
         Terraform = "true"
         Project = "Roboshop"
         Environment = "dev"
-
     }
+}
 
+variable "sg_name" {
+    type = string
+    default = "allow_all1"
+    description = Security group name for an EC2 instance"
+}
+
+variable "cidr" {
+    type = list
+    default = ["0.0.0.0/0"]
+    
+}
+
+variable "ingress_from_port" {
+    type = number
+    default = 0 
+}
+
+variable "ingress_to_port" {
+    default = 0 
+}
+
+variable "egress_from_port" {
+    default = 0 
+}
+
+variable "egress_to_port" {
+    default = 0 
+}
+
+variable "protocol" {
+    type = string
+    default = "-1"
 }
