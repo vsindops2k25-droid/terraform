9a814738c79925932ec910c2d1a958a5eb2ee771
Vijaya Kumar Sinthu - (Internet) <vijayakumar.sinthu@mobileheartbeat.com>
2025-10-24 09:29:03 +0530

    Variables & Datatypes

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -2,11 +2,7 @@ resource "aws_instance" "terraform" {
   ami           = var.aws_id
   instance_type = var.instnace_type
   vpc_security_group_ids = [aws_security_group.allow_all1.id]
-
-  tags = {
-    Name = "Terraform"
-    Terraform = "true"
-  }
+  tags = var.ec2_tags
 }
 
 resource "aws_security_group" "allow_all1" {
--- variables/variables.tf
+++ variables/variables.tf
@@ -1,7 +1,21 @@
 variable "aws_id" {
+    type = string
     default = "ami-09c813fb71547fc4f"
-}
+ }
 
 variable "instnace_type" {
+    type = string
     default = "t3.micro"
+}
+
+variable "ec2_tags"{
+    type = map
+    default = {
+        Name = "Terraform-demo"
+        Terraform = "true"
+        Project = "Roboshop"
+        Environment = "dev"
+
+    }
+
 }
